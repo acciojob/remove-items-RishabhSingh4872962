a12126faa1c47dd119db5093b8f57bc41ea981a7
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-02-24 11:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+ 
+const colorSelect=document.getElementById("colorSelect");
+ 
+function remove() {
+	let indx=null;
+	for (let op of colorSelect) {
+		if (colorSelect.value==op.innerText) {
+			op.remove()
+		}
+	}
+}
